cee056ebc136b29483f56e62da3c187c09bfd823
Varad <vaidyavarad2001@gmail.com>
2021-03-16 19:26:04 +0530

    added trajecotry control over trajectory control

--- tests/jointSpaceControl.py
+++ tests/jointSpaceControl.py
@@ -19,25 +19,35 @@ jointAngles = np.array(jointAngles)
 robot.turnOFFActuators()
 robot.turnOFFInternalDamping()
 
-desJointAngles = np.array([-2,-2.7,2.4,0,-1.57,-1.57])
+desJointAngles = [0,-1.57,1.57,-1.57,-1.57,-1.57]
 
+# sim time parameters
+simTime = 40 # sec
+trajTime = 15 #sec
+timeSteps = simTime * 240
+time = np.linspace(0,simTime,num=timeSteps)
+
+robot.getJointsTrajectory(jointAngles,desJointAngles,trajTime,time)
 desEndEffectorVel = np.array([0,0,0,0,0,0])
 
-Kp = np.diag(6*[1])
-Kd = np.diag(6*[1])
+Kp = np.diag(6*[4])
+Kd = np.diag(6*[0])
 
-while True:
+for i in range(len(time)):
     robot.getJointInfo()
     robot.getForwardKinematics()
     robot.calculateJacobian()
     robot.getDyanamicMatrices()
     
-    jointPositonError = desJointAngles - robot.jointState.jointAngles
-    print(jointPositonError)
-    jointVelocityError = desEndEffectorVel - robot.jointState.jointVelocities
+    jointPositonError = robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles
+    robot.plotError.append(jointPositonError)
+    jointVelocityError = robot.TrajectoryPlan.jointVelTraj[:,i] - robot.jointState.jointVelocities
     
     torque = Kp.dot(jointPositonError) + Kd.dot(jointVelocityError) + robot.DynamicMatrices.gravityVector
     
     pb.setJointMotorControlArray(robot.armID,robot.controlJoints,pb.TORQUE_CONTROL, forces = torque)
     pb.stepSimulation()
     
+plotError = np.array(robot.plotError)
+print("i reached here")
+robot.plotValues(plotError,time)
