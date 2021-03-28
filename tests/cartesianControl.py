d5254a0bb0151291459b4a1dc2bf00a272ffbd13
Varad Vaidya <vaidyavarad2001@gmail.com>
2021-03-29 00:28:13 +0530

    added quaternion error and proper tuning

--- tests/cartesianControl.py
+++ tests/cartesianControl.py
@@ -7,51 +7,64 @@ if (not (path2add in sys.path)) :
 from manipulator import Manipulator
 import numpy as np
 import pybullet as pb
-
+import pybullet_utils.transformations as trans
+#from ..manipulator.manipulator import Manipulator
 
 ## set the style for matplotli
 robot = Manipulator()
 pb.setRealTimeSimulation(False)
-jointAngles = [1.57,-1.7,2.4,-1.57,-1.57,-1.57]
+jointAngles = [0,-1.57,1.57,1,1.,1.]
 robot.setJointAngles(jointAngles)
 
 robot.turnOFFActuators()
 robot.turnOFFInternalDamping()
-desEndEffector = np.array([0.7,0.2,1.5,0.2,1.2,0.5])
+robot.getJointInfo()
+robot.getForwardKinematics()
+
+desEndEffector = desEndEffector = np.array([0.3,0.2,1.5,2,0.2,1.2])
+droll,dpitch,dyaw = desEndEffector[3:6]
+desQuat = trans.unit_vector(trans.quaternion_from_euler(droll,dpitch,dyaw))
+
 desEndEffectorVel = np.array([0,0,0,0,0,0])
 
-Kp = np.diag([9,9,9,0.3,.3,.3])
-Kd = np.diag([9,9,9,0,0,0])
+Kp = np.diag([2.5,2.5,2.5,0.08,0.08,0.08])
+Kd = np.diag([8,8,8,0.115,0.115,0.115])
 
 
 # sim time parameters
-simTime = 320 # sec
+simTime = 100 # sec
 timeSteps = simTime * 240
 time = np.linspace(0,simTime,num=timeSteps)
 
 for i in range(len(time)):
 
-
-   
     robot.getJointInfo()
     robot.getForwardKinematics()
     robot.calculateJacobian()
     robot.getDyanamicMatrices()
     
     
-    endEffectorVel = robot.Jacobian.analyticJacobian.dot(robot.jointState.jointVelocities)
+    endEffectorVel = robot.Jacobian.geometricJacobian.dot(robot.jointState.jointVelocities)
+    #print(endEffectorVel)
+    positionError = desEndEffector[0:3] - np.array(robot.forwardKinematics.linkPosition)
     
-    positionError = desEndEffector - np.array(robot.forwardKinematics.linkState)
+    currQuat = trans.unit_vector(robot.forwardKinematics.linkOrientationQuaternion)
     
-    robot.plotError.append(positionError)
+    errorQuat = trans.quaternion_multiply(desQuat,trans.quaternion_conjugate(currQuat))
+    
+    orientationError = errorQuat[0:3] * np.sign(errorQuat[3])
+    
+    posError = np.hstack((positionError,orientationError))
+    
+    #print("Pos Error",posError)
+    robot.plotError.append(posError)
     
     velocityError = desEndEffectorVel - endEffectorVel
     
-    commanadedForce = Kp.dot(positionError) + Kd.dot(velocityError)
+    commanadedForce = Kp.dot(posError) + Kd.dot(velocityError)
     
-    commanadedJointTorque = robot.Jacobian.analyticJacobian.T.dot(commanadedForce) + robot.DynamicMatrices.gravityVector
-    
-    
+    commanadedJointTorque = robot.Jacobian.geometricJacobian.T.dot(commanadedForce) + robot.DynamicMatrices.gravityVector
+    # robot.Jacobian.analyticJacobian.T.dot(commanadedForce)
     pb.setJointMotorControlArray(robot.armID,robot.controlJoints,pb.TORQUE_CONTROL, forces = commanadedJointTorque)
     pb.stepSimulation()
     
