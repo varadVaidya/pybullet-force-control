db037bfc91ce3d47f923a02e580a86215222f0a2
Varad Vaidya <vaidyavarad2001@gmail.com>
2021-04-29 22:40:13 +0530

    added live plot to test this  mode

--- tests/compTorqueControl.py
+++ tests/compTorqueControl.py
@@ -15,8 +15,8 @@ robot.setJointAngles(initjointAngles)
 finalJointAngles = [0,0,1.4,0.,0.,-1]
 
 # sim time parameters
-simTime = 30 # sec
-trajTime = 10 #sec
+simTime = 100 # sec
+trajTime = 50 #sec
 timeSteps = simTime * 240
 time = np.linspace(0,simTime,num=timeSteps)
 
@@ -29,24 +29,32 @@ robot.turnOFFInternalDamping()
 robot.getJointsTrajectory(initjointAngles,finalJointAngles,trajTime,time)
 
 print("final teaj is : ", robot.TrajectoryPlan.jointAngleTraj[:,-1])
-for i in range(len(time)):
-    
-    robot.getJointInfo()
-    robot.getForwardKinematics()
-    robot.calculateJacobian()
-    robot.getDyanamicMatrices()
-    
-    desiredAccel = robot.TrajectoryPlan.jointAccelTraj[:,i]
-    positionError = Kp.dot(robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles)
-    robot.plotError.append(robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles)
-    velocityError = Kd.dot(robot.TrajectoryPlan.jointVelTraj[:,i] - robot.jointState.jointVelocities)
-    
-    inputAccel = desiredAccel + positionError + velocityError
-    
-    torque = robot.DynamicMatrices.massMatrix.dot(inputAccel) + robot.DynamicMatrices.coriolisVector + robot.DynamicMatrices.gravityVector
-    
-    pb.setJointMotorControlArray(robot.armID,robot.controlJoints,pb.TORQUE_CONTROL,forces = torque)
-    pb.stepSimulation()
+
+f = open("plot.csv" , "w")
+f.truncate()
+f.close()
+
+with open("plot.csv",'a') as f:
+    for i in range(len(time)):
+        
+        robot.getJointInfo()
+        robot.getForwardKinematics()
+        robot.calculateJacobian()
+        robot.getDyanamicMatrices()
+        
+        desiredAccel = robot.TrajectoryPlan.jointAccelTraj[:,i]
+        positionError = Kp.dot(robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles)
+        robot.plotError.append(robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles)
+        posError = robot.TrajectoryPlan.jointAngleTraj[:,i] - robot.jointState.jointAngles
+        np.savetxt(f, posError.reshape(1,6),newline='\n',fmt='%f',delimiter=',')
+        velocityError = Kd.dot(robot.TrajectoryPlan.jointVelTraj[:,i] - robot.jointState.jointVelocities)
+        
+        inputAccel = desiredAccel + positionError + velocityError
+        
+        torque = robot.DynamicMatrices.massMatrix.dot(inputAccel) + robot.DynamicMatrices.coriolisVector + robot.DynamicMatrices.gravityVector
+        
+        pb.setJointMotorControlArray(robot.armID,robot.controlJoints,pb.TORQUE_CONTROL,forces = torque)
+        pb.stepSimulation()
     
 plotError = np.array(robot.plotError)
 print("i reached here")
