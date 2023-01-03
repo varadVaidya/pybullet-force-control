5ac41c2c3454d31ec8b35c7b20340359655004c8
Varad Vaidya <vaidyavarad2001@gmail.com>
2023-01-03 18:38:37 +0530

    I FOUND MY FILESSSS

--- tests/impedance.py
+++ tests/impedance.py
@@ -12,8 +12,11 @@ import pybullet_utils.transformations as trans
 #from ..manipulator.manipulator import Manipulator
 ## set the style for matplotlib
 robot = Manipulator()
+table = pb.loadURDF("table/table.urdf", [0.,0.6,0], pb.getQuaternionFromEuler([0,0,0]))
+pb.changeDynamics(table,-1,restitution = 0.9,lateralFriction = 0)
+
 pb.setRealTimeSimulation(False)
-jointAngles = [0,-2,2,-1.57,-1.57,-1.57]
+jointAngles = [1,0,0.8,-2.37,-1.57,-1.57]
 robot.setJointAngles(jointAngles)
 
 robot.turnOFFActuators()
@@ -34,11 +37,18 @@ desEndEffectorVel = np.array([0,0,0,0,0,0])
 
 # sim time parameters
 simTime = 400 # sec
+trajTime = 30 #sec
 timeSteps = simTime * 240
 time = np.linspace(0,simTime,num=timeSteps)
 
 
-K_MATRIX = np.diag([100,100,100,100,100,100])
+point1 = robot.Waypoint(position=robot.forwardKinematics.linkPosition,velocity=[0,0,0],acceleration=[0,0,0])
+point2 = robot.Waypoint(position=[-0.2,0.5,0.6],velocity=[0,0,0],acceleration=[0,0,0])
+
+positionTrajectory,velocityTrajectory,accelerationTrajectory = robot.getCartesianTraj(point1,point2,trajTime,time)
+pb.enableJointForceTorqueSensor(robot.armID,robot.endEffectorIndex,True)
+
+K_MATRIX = np.diag([500,500,500,500,500,500])
 D_MATRIX = 2 * np.sqrt(K_MATRIX)
 #DES_INERTIA = robot.DynamicMatrices.cartesianMassMatrix
 DES_INERTIA = 0.1 * np.eye(6)
@@ -53,13 +63,13 @@ for i in range(len(time)):
     robot.calculateJacobian()
     robot.getDyanamicMatrices()
     
+    desVel = np.hstack((velocityTrajectory[i],[0,0,0]))
     
     F_ext = robot.readGUIparams(ForceGUIids) # applied external forces
     
     endEffectorVel = robot.Jacobian.geometricJacobian.dot(robot.jointState.jointVelocities)
     #print(endEffectorVel)
-    positionError = desEndEffector[0:3] - np.array(robot.forwardKinematics.linkPosition)
-    
+    positionError = positionTrajectory[i] - np.array(robot.forwardKinematics.linkPosition)
     currQuat = trans.unit_vector(robot.forwardKinematics.linkOrientationQuaternion)
     
     errorQuat = trans.quaternion_multiply(desQuat,trans.quaternion_conjugate(currQuat))
@@ -71,7 +81,7 @@ for i in range(len(time)):
     #print("Pos Error",posError)
     #robot.plotError.append(posError)
     
-    velocityError = desEndEffectorVel - endEffectorVel
+    velocityError = desVel - endEffectorVel
     
     springForce = K_MATRIX.dot(posError) + D_MATRIX.dot(velocityError)
     
